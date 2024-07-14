4bfd5e623044336f3d6d437938dd44a102551ebb
medmsf <mohamedmoussaaif5@gmail.com>
2024-07-14 01:08:17 +0100

    add ghana league

--- src/Data/Topleagues/TopleaguesData.js
+++ src/Data/Topleagues/TopleaguesData.js
@@ -2,11 +2,19 @@ const leagues =
 [
     {
         leagueId : 1,
-        leagueName : "Champions League",
-        leagueLogo : "https://seeklogo.com/images/U/UEFA_Champions_League-logo-DD9AE0500D-seeklogo.com.png",
-        countryName : "Europe",
+        leagueName : "Non Premier League",
+        leagueLogo : "https://apiv3.apifootball.com/badges/logo_leagues/149_non-league-premier.png",
+        countryName : "England",
         countryLogo : 'https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/1200px-Flag_of_England.svg.png',
         api_id : 149 ,
+    },
+    {
+        leagueId : 2,
+        leagueName : "Premier League Ghana",
+        leagueLogo : "https://apiv3.apifootball.com/badges/logo_leagues/177_premier-league.png",
+        countryName : "Ghana",
+        countryLogo : 'https://apiv3.apifootball.com/badges/logo_country/50_ghana.png',
+        api_id : 177 ,
     },
 ]
 
